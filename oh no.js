5490c7089d27c103388622ca225473334fd9b1c0
codeinngincode <66986522+codeinngincode@users.noreply.github.com>
2022-04-13 12:51:26 +0530

    Add files via upload

---
+++ oh no.js
@@ -0,0 +1,8 @@
+function hiphen_underscore() {
+				 	var player1_name=document.getElementById("idotthe2th").value;
+				 	var player2_name=document.getElementById("idotthe2th2th").value;
+				 	localStorage.setItem("t5_reqi",t5_reqi);
+				 	localStorage.setItem("t6_reqi",t6_reqi);
+				 	window.location="keqi wxev uym3";
+
+				 } 
